b98040fbdb914b51dd668306f63a4a590d79e0da
Timo Mehring <timo0mehring@gmail.com>
2026-09-08 20:45:23 +0200

    [Updated] Battle::LoadMonsterDate added second monster, Battle::StartTestEncounter random monster selection

--- src/Battle.cpp
+++ src/Battle.cpp
@@ -7,10 +7,14 @@ Battle::Battle(){
 
 void Battle::LoadMonsterData(){
     monsterData.push_back({"Plant001", 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, "assets/MonsterSprites/plant001_front.png", "assets/MonsterSprites/plant001_back.png"});
+    monsterData.push_back({"Plant002", 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, "assets/MonsterSprites/plant002_front.png", "assets/MonsterSprites/plant002_back.png"});
 }
 
 void Battle::StartTestEncounter(){
-    MonsterData& data = monsterData[0];
+    int randomIndex = GetRandomValue(0, static_cast<int>(monsterData.size()) -1);
+
+
+    MonsterData& data = monsterData[randomIndex];
     currentMonster = new Monster(data.name, data.range, data.speed, data.health, data.resistance, data.attack, data.frontSpritePath, data.backSpritePath);
 }
 
